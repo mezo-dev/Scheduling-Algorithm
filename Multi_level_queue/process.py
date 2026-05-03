61f27efa5bc2ba337b0125f2ad441052ef5a0509
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-04 00:13:56 +0300

    feat: created process

---
+++ Multi_level_queue/process.py
@@ -0,0 +1,16 @@
+
+
+
+class Process:
+    _next_id = 1
+
+    def __init__(self, arrival_time: int, burst_time: int) -> None:
+        self.id = Process._next_id
+        Process._next_id += 1
+        self.arrival_time = arrival_time
+        self.burst_time = burst_time
+
+
+    @classmethod
+    def reset_ids(cls) -> None:
+        cls._next_id = 1
