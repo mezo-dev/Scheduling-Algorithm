5ae4cea50af5d3beddf9bab798cf74fd41436960
Moataz0000 <moatazfawzy150@gmail.com>
2026-05-04 00:31:36 +0300

    feat: process state

--- Multi_level_queue/process.py
+++ Multi_level_queue/process.py
@@ -1,3 +1,6 @@
+from .states import State
+
+
 class Process:
     _next_id = 1
 
@@ -6,7 +9,17 @@ class Process:
         Process._next_id += 1
         self.arrival_time = arrival_time
         self.burst_time = burst_time
+        self.state = State.READY
 
     @classmethod
     def reset_ids(cls) -> None:
         cls._next_id = 1
+
+    def mark_as_running(self):
+        self.state = State.RUNNING
+
+    def mark_as_waiting(self):
+        self.state = State.WAITING
+
+    def mark_as_terminated(self):
+        self.state = State.TERMINATED
